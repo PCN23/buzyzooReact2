76521f5b760aa0d3b33db71f36537e37e5dd9930
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-30 14:48:44 -0700

    2nd test working

--- src/AnimalEmoji.test.js
+++ src/AnimalEmoji.test.js
@@ -2,8 +2,15 @@ import { render, screen } from '@testing-library/react';
 import AnimalEmoji from './AnimalEmoji';
 // import App from './App';
 
+// unit test
 test('if we pass a crocodile to AnimalEmoji, it renders a crocodile emoji', () => {
   render(<AnimalEmoji animal='crocodiles'/>);
   const crocodileElement = screen.getByText(/🐊/i);
   expect(crocodileElement).toBeInTheDocument();
+});
+
+test('if we pass a invalid animal to AnimalEmoji, it should render an empty div', () => {
+  render(<AnimalEmoji animal='deer'/>);
+  const crocodileElement = screen.queryByText('🐊');
+  expect(crocodileElement).not.toBeInTheDocument();
 });
