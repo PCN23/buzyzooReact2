a7192e2c1e3bb9f7a7b78677bba680d73053abe2
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-07-01 10:50:01 -0700

    added buttons and are working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import OpenClosedSign from './OpenClosedSign';
 import AnimalList from './AnimalList';
+import CustomButton from './CustomButton';
 
 function App() {
 
@@ -41,27 +42,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to the costa rican zoo. </h1>
         <div className='pokemon'>
           <div className='competitor'>
             <p style={{ fontSize: `${carSize}px` }}>🐝 : {carSize}</p>
-            <button onClick={() => setCarSize(carSize + 10)}>Grow Bee</button>
-            <button onClick={() => setCarSize(carSize - 10)}>Shrink Bee</button>
+            <CustomButton onClick={() => setCarSize(carSize + 10)}>Grow Bee</CustomButton>
+            <CustomButton onClick={() => setCarSize(carSize - 10)}>Shrink Bee</CustomButton>
           </div>
           <div className='competitor'>
             <p style={{ fontSize: `${truckSize}px` }}>🦜 : {truckSize}</p>
-            <button onClick={() => setTruckSize(truckSize + 10)}>Grow Parrot</button>
-            <button onClick={() => setTruckSize(truckSize - 10)}>Shrink Parrot</button>
+            <CustomButton onClick={() => setTruckSize(truckSize + 10)}>Grow Parrot</CustomButton>
+            <CustomButton onClick={() => setTruckSize(truckSize - 10)}>Shrink Parrot</CustomButton>
           </div>
         </div>
         <OpenClosedSign zooIsOpen={zooIsOpen}/>
-        <button onClick={() => setZooIsOpen(!zooIsOpen)}>toggle zoo</button>
+        <CustomButton onClick={() => setZooIsOpen(!zooIsOpen)}>toggle zoo</CustomButton>
         <AnimalList animals={animals}/>
-        <button onClick={handleMacawClick}>Add macaw</button>
-        <button onClick={handleSlothClick}>Add sloth</button>
-        <button onClick={handleCoatiClick}>Add coati</button>
-        <button onClick={handleToucansClick}>Add toucans</button>
-        <button onClick={handleCrocodilesClick}>Add crocodiles</button>
-        <button onClick={handleRetireAnimal}>Goodbye animal</button>      
+        <CustomButton onClick={handleMacawClick}>Add macaw</CustomButton>
+        <CustomButton onClick={handleSlothClick}>Add sloth</CustomButton>
+        <CustomButton onClick={handleCoatiClick}>Add coati</CustomButton>
+        <CustomButton onClick={handleToucansClick}>Add toucans</CustomButton>
+        <CustomButton onClick={handleCrocodilesClick}>Add crocodiles</CustomButton>
+        <CustomButton onClick={handleRetireAnimal}>Goodbye animal</CustomButton>      
       </header>
     </div>
   );
---
+++ src/CustomButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+export default function CustomButton({ onClick, children }) {
+  return (
+    <Button onClick={onClick} variant="contained">{children}</Button>
+
+  );
+}
+
+
